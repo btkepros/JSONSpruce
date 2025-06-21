1cc58a342d95c461560417c1debdf99846ac2a2f
btkepros <btkepros@gmail.com>
2025-06-21 01:06:30 -0400

    Add files via upload

---
+++ JSONify/JSONifyTests/JSONifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  JSONifyTests.swift
+//  JSONifyTests
+//
+//  Created by Brandon Kepros on 6/20/25.
+//
+
+import Testing
+@testable import JSONify
+
+struct JSONifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
